05642d56fe3b8543f228f93d13e63ef8d6464ec5
Michael Bessey <besseym@gmail.com>
2015-12-02 23:46:54 -0500

    Select View behavior

--- scripts/view/SelectView.js
+++ scripts/view/SelectView.js
@@ -1,5 +1,7 @@
-define(["common", "view/TemplateView"],
-    function (common, TemplateView) {
+define(
+    ["common", "courier", "view/TemplateView"],
+
+    function (common, courier, TemplateView) {
 
         var SelectView = function (config, parent) {
 
@@ -14,6 +16,35 @@ define(["common", "view/TemplateView"],
 
             function updateView(data){
 
+                var view = parent.getView(),
+                    linkTable = view.find("#s-n-c-tbl"),
+                    linkArray = linkTable.find('.s-l');
+                    linkBtnArray = linkTable.find('.s-l-btn');
+
+                linkArray.on("mouseover", function() {
+
+                    var link = $(this),
+                        lId = link.data('link-id'),
+                        targetId = link.data('target-id');
+
+                    courier.publish("view-hover-link", {
+                        lId: lId
+                    });
+                });
+
+                linkArray.on("mouseout", function() {
+
+                });
+
+                linkBtnArray.on("mouseover", function() {
+
+                    $(this).children('i').removeClass( "fa-link" ).addClass( "fa-chain-broken" );
+                });
+
+                linkBtnArray.on("mouseout", function() {
+
+                    $(this).children('i').removeClass( "fa-chain-broken" ).addClass( "fa-link" );
+                });
             }
 
             /***** public methods *****/
